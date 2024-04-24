31047dac003448308764dcb1758e1fc9b4089a02
natalia9519 <nataliayda@gmail.com>
2024-04-24 11:36:03 +0200

    new file:   client/public/test1.png 	new file:   server/__tests__/auth.controller.test.js 	new file:   server/__tests__/bookController.test.js 	new file:   server/__tests__/contactController.test.js 	new file:   server/__tests__/eventController.test.js 	modified:   server/index.js 	modified:   server/package-lock.json 	modified:   server/package.json

--- server/index.js
+++ server/index.js
@@ -5,6 +5,10 @@ import bookRoutes from './routes/bookRoutes.js';
 import authRoutes from './routes/authRoutes.js';
 import eventRoutes from './routes/eventRoutes.js';
 import contactRoutes from './routes/contactRoutes.js';
+import path from 'path';
+
+
+
 
 const app = express();
 app.use(cors());
@@ -15,11 +19,16 @@ app.use("/user", authRoutes);
 app.use("/event", eventRoutes);
 app.use("/contact",contactRoutes);
 
+app.use(express.static(path.join(dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+})
 
 export { app };
-
 app.listen(8000, () => {
   console.log("Conectado");
 });
 
 db();
+const dirname = path.resolve();
