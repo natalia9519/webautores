c0676f95b8583822a74187104117f822c272fa28
natalia9519 <nataliayda@gmail.com>
2024-04-24 13:59:47 +0200

    modified:   app/index.js

--- app/index.js
+++ app/index.js
@@ -62,7 +62,7 @@ app.use("/contact", contactRoutes);
 app.use(express.static(path.join(__dirname, 'client', 'dist')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+  res.sendFile(path.join(__dirname, 'client', 'public', 'index.html'));
 });
 
 export { app };
