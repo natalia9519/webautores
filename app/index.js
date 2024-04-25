1d3c4ae42eec3845bbf0761ba1ceccdde7c26d43
natalia9519 <nataliayda@gmail.com>
2024-04-25 09:00:15 +0200

    modified:   app/index.js

--- app/index.js
+++ app/index.js
@@ -33,43 +33,34 @@
 
 
 
-
-
-
-import { db } from "./database/db.js";
-import cors from 'cors';
+// Importar las dependencias necesarias
 import express from "express";
-import bookRoutes from './routes/bookRoutes.js';
-import authRoutes from './routes/authRoutes.js';
-import eventRoutes from './routes/eventRoutes.js';
-import contactRoutes from './routes/contactRoutes.js';
-import { fileURLToPath } from 'url';
-import path from 'path';
+import path from "path";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
+// Crear una instancia de Express
 const app = express();
-app.use(cors());
-app.use(express.json());
+
+// Rutas para el backend
+import bookRoutes from "./routes/bookRoutes.js";
+import authRoutes from "./routes/authRoutes.js";
+import eventRoutes from "./routes/eventRoutes.js";
+import contactRoutes from "./routes/contactRoutes.js";
 
 app.use("/book", bookRoutes);
 app.use("/user", authRoutes);
 app.use("/event", eventRoutes);
 app.use("/contact", contactRoutes);
 
-app.use(express.static(path.join(__dirname, '..', 'client', 'dist')));
+// Servir archivos estáticos del frontend
+app.use(express.static(path.join(__dirname, "../client/build")));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'client', 'public', 'index.html'));
+// Ruta para todas las demás solicitudes
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build", "index.html"));
 });
 
-db().then(() => {
-  const PORT = 8000;
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
-}).catch((error) => {
-  console.log(error);
-  process.exit(1);
+// Iniciar el servidor
+const PORT = 8000;
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
